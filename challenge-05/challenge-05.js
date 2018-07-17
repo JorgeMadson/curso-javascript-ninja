bad1a5b8f936740d7ae3f691fa1a2e457797bd64
Jorge Madson <filholouco@gmail.com>
2018-07-17 00:52:48 -0300

    não terminado
    a função não tava muito bem explicada.

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var qualquer = [ 1, 'jorge', null, 1.67, 'sei la'];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function retornaArray (vetor) {
+  return vetor; 
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+retornaArray(qualquer)[1];
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,24 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function doisParametros(vetor, indice) {
+  return vetor[indice];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var diferente = [ 1, 'jorge', null, {num: 1}, NaN];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+doisParametros(diferente, 0); //1
+doisParametros(diferente, 1); //"jorge"
+doisParametros(diferente, 2); //null
+doisParametros(diferente, 3); //{num: 1}
+doisParametros(diferente, 4); //NaN
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,7 +55,13 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(nome) {
+  var objeto = {
+   quantidadePaginas: Number,
+   autor: String,
+   editora: String
+  };
+};
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
